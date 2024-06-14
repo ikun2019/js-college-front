fda4c2a1f8b298232cdce627c056d7fbcb45a3a2
ikun <0215.miyatto@gmail.com>
2024-06-14 23:17:56 +0900

    headerの追加

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Link from 'next/link';
+
+import { Button } from '../ui/button';
+import { HomeIcon, NewspaperIcon } from '@heroicons/react/16/solid/index.js';
+
+const Header = () => {
+	return (
+		<header className="font-mono flex justify-between py-3 px-6">
+			<div>
+				<p className="font-sans text-sm text-gray-500">JavaScriptを学ぶなら</p>
+				<p className="text-xl">JS College</p>
+			</div>
+			<nav className="flex items-center">
+				<ul className="flex items-center">
+					<li>
+						<Button variant="ghost" asChild>
+							<Link href="/" className="text-gray-600">
+								<HomeIcon className="size-4" />
+								Home
+							</Link>
+						</Button>
+					</li>
+					<li>
+						<Button variant="ghost" asChild>
+							<Link href="/blogs" className="text-gray-600">
+								<NewspaperIcon className="size-4" />
+								Blogs
+							</Link>
+						</Button>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/pages/blogs/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Image from 'next/image';
+
+const BlogsPage = ({ metas }) => {
+	console.log('data =>', metas);
+	return (
+		<>
+			<main>
+				<section className="relative">
+					<Image
+						src="/hero.jpg"
+						alt="hero画像"
+						width={1000}
+						height={500}
+						layout="responsive"
+						className="max-h-72"
+					/>
+					<div className="absolute inset-0 h-full bg-black bg-opacity-40"></div>
+					<div className="absolute inset-0 h-full flex items-center justify-center">
+						<div>
+							<h1 className="text-white text-3xl font-bold">
+								Engineering blog for individual developers
+							</h1>
+						</div>
+					</div>
+				</section>
+				<div className="flex content-center">
+					<section className="container mt-6 mx-auto">
+						<h2 className="font-bold text-2xl text-left">Latest Strories</h2>
+					</section>
+				</div>
+			</main>
+		</>
+	);
+};
+
+export async function getServerSideProps() {
+	try {
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/blogs`);
+		const data = await response.json();
+		return {
+			props: {
+				metas: data,
+			},
+		};
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export default BlogsPage;
