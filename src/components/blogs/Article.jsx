4ca9d1eeb7bfc677f9801a7b7e85356f23fb234d
ikun <0215.miyatto@gmail.com>
2024-06-16 22:07:38 +0900

    トップページにタグを表示

--- src/components/blogs/Article.jsx
+++ src/components/blogs/Article.jsx
@@ -21,12 +21,23 @@ const Article = ({ meta }) => {
 							</span> */}
 						</div>
 						<div className="p-4 flex-grow">
-							<h3 className="text-lg font-bold mb-2">{meta.title}</h3>
+							<h3 className="font-bold mb-2">{meta.title}</h3>
 							{/* <p className="text-gray-700 mb-2">1,724 Reviews</p> */}
 							{/* <p className="text-gray-700 mb-2">
               <span className="line-through">$150</span> <span className="font-bold">$130</span>
             </p> */}
-							<p className="text-gray-700 mb-4">{meta.description}</p>
+							<div className="space-x-2 mb-3">
+								{meta.tags &&
+									meta.tags.map((tag) => (
+										<span
+											key={tag}
+											className="px-4 py-2 bg-gray-800 text-white rounded-full text-sm"
+										>
+											{tag}
+										</span>
+									))}
+							</div>
+							<p className="text-gray-700 mb-4 text-sm">{meta.description}</p>
 						</div>
 						<div className="mt-auto">
 							<span className="text-gray-500">{meta.date}</span>
