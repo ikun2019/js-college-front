c3c678712df4bd409fce7de850c127fd75534bdf
ikun <0215.miyatto@gmail.com>
2024-07-07 14:02:23 +0900

    courseページのUI変更

--- src/pages/learnings/[slug]/index.jsx
+++ src/pages/learnings/[slug]/index.jsx
@@ -20,21 +20,21 @@ const index = ({ parentMetadata, childMetadatas }) => {
 				<section className="flex flex-wrap -mx-6">
 					{/* Learnings Page */}
 					<div className="w-full lg:w-3/4 px-6">
-						<div className="bg-white p-6 rounded-lg shadow-lg">
-							<h1 className="text-3xl font-bold mb-6 font-serif">{parentMetadata.title}</h1>
+						<div className="bg-white p-8 rounded-lg shadow-lg">
+							<h1 className="text-4xl font-bold mb-6 text-gray-800">{parentMetadata.title}</h1>
 							<ReactMarkdown
 								children={parentMetadata.content}
 								components={{
 									h2(props) {
 										return (
-											<h2 className="text-2xl border-b-4 border-gray-500 pl-4 mb-4">
+											<h2 className="text-2xl border-b-4 border-blue-500 pl-4 mb-4">
 												{props.children}
 											</h2>
 										);
 									},
 									h3(props) {
 										return (
-											<h3 className="text-xl border-l-4 border-gray-500 pl-4 mb-4 mt-3">
+											<h3 className="text-xl border-l-4 border-blue-500 pl-4 mb-4 mt-3">
 												{props.children}
 											</h3>
 										);
@@ -44,16 +44,16 @@ const index = ({ parentMetadata, childMetadatas }) => {
 										if (node.children[0].tagName === 'img') {
 											const image = node.children[0];
 											return (
-												<div className="relative w-full">
+												<div className="relative w-full my-4">
 													<img
 														src={image.properties.src}
 														alt={image.properties.alt}
-														className="object-cover"
+														className="object-cover rounded-lg shadow-lg"
 													/>
 												</div>
 											);
 										}
-										return <p>{paragraph.children}</p>;
+										return <p className="text-lg leading-relaxed mb-4">{paragraph.children}</p>;
 									},
 									code(props) {
 										const { children, className } = props;
@@ -64,32 +64,38 @@ const index = ({ parentMetadata, childMetadatas }) => {
 												children={String(children).replace(/\n$/, '')}
 												language={language}
 												style={dracula}
-												customStyle={{ fontSize: '0.8em' }}
+												customStyle={{ fontSize: '0.9em', borderRadius: '8px' }}
 											/>
 										) : (
-											<code className={`${className} text-sm`}>{children}</code>
+											<code className={`${className} text-sm bg-gray-100 p-1 rounded`}>
+												{children}
+											</code>
 										);
 									},
 								}}
 								className="text-gray-700 mb-4"
 							></ReactMarkdown>
-							<ul>
+							<ul className="grid grid-cols-1 md:grid-cols-2 gap-6">
 								{childMetadatas.map((item, index) => (
 									<li key={index} className="mb-3">
-										<div>
+										<div className="bg-gray-100 p-4 rounded-lg hover:shadow-md transition-shadow duration-300">
 											<Link
 												href={`/learnings/${parentMetadata.slug}/${item.slug}`}
-												className="hover:underline"
+												className="cursor-pointer"
 											>
-												<h2 className="text-xl font-bold mb-2">{item.title}</h2>
+												<h2 className="text-xl font-bold mb-2 text-gray-700">{item.title}</h2>
+												<p className="text-gray-600">{item.description}</p>
 											</Link>
-											<p className="ml-6">{item.description}</p>
 										</div>
 									</li>
 								))}
 							</ul>
 						</div>
 					</div>
+					{/* Sidebar */}
+					{/* <div className="w-full lg:w-1/4 px-6 mt-8 lg:mt-0">
+						<Sidebar />
+					</div> */}
 				</section>
 			</div>
 		</>
