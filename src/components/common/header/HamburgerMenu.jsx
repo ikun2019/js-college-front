af397e9e59127445b6a4265ba64cb748adff65c0
ikun <0215.miyatto@gmail.com>
2024-07-23 09:40:39 +0900

    fixed

--- src/components/common/header/HamburgerMenu.jsx
+++ src/components/common/header/HamburgerMenu.jsx
@@ -12,20 +12,20 @@ const HamburgerMenu = ({ toggleHamburger, isActive, user, onSignout }) => {
 				onClick={toggleHamburger}
 			>
 				<span
-					className={`inline-block absolute h-[2px] bg-gray-50 w-[62%] left-[10px] transition-all top-[13px] ${
-						isActive ? 'top-[18px] left-[18px] translate-y-[6px] rotate-[-45deg] w-[30%]' : ''
+					className={`block absolute h-[2px] bg-gray-50 w-[62%] left-[10px] transition-all duration-300 ease-in-out ${
+						isActive ? 'top-[24px] left-[13px] rotate-[-45deg] w-[50%]' : 'top-[14px]'
 					}`}
 				></span>
 				<span
-					className={`inline-block transition-all absolute top-[18px] left-[11px] text-xs mx-auto text-gray-50 ${
-						isActive ? 'opacity-0' : ''
+					className={`block transition-all absolute text-center w-full text-xs mx-auto text-gray-50 ${
+						isActive ? 'opacity-0' : 'top-[18px]'
 					}`}
 				>
 					Menu
 				</span>
 				<span
-					className={`inline-block absolute h-[2px] bg-gray-50 w-[62%] left-[10px] transition-all top-[36px] ${
-						isActive ? 'top-[30px] left-[18px] translate-y-[-6px] rotate-[45deg] w-[30%]' : ''
+					className={`block absolute h-[2px] bg-gray-50 w-[62%] left-[10px] transition-all duration-300 ease-in-out ${
+						isActive ? 'top-[24px] left-[13px] rotate-[45deg] w-[50%]' : 'top-[36px]'
 					}`}
 				></span>
 			</div>
@@ -70,7 +70,7 @@ const HamburgerMenu = ({ toggleHamburger, isActive, user, onSignout }) => {
 						</>
 					) : (
 						<li className="py-2 block w-full hover:bg-gray-200">
-							<Link href="/auth/signin" className="text-gray-100 font-bold bg-blue-500">
+							<Link href="/auth/signin" className="block w-full px-4 py-2 text-center">
 								SignIn
 							</Link>
 						</li>
